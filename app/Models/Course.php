d03c1c7414e3cc4b30821da7da323bc10ace92d8
*Anuradha-DBT* <*anuradham.dbt@gmail.com*>
2025-08-14 18:14:10 +0530

    create course model and controller

---
+++ app/Models/Course.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+     protected $table = 'courses';
+    protected $fillable = [
+        'title',
+        'slug',
+        'short_description',
+        'about_course',
+        'why_join_the_course',
+        'instructor_name',
+        'instructor_designation',
+        'instructor_details',
+        'mrp',
+        'sellable_price',
+        'is_popular',
+        'show_on_home',
+        'featured_image',
+        'is_certificate',
+        'certificate_file',
+        'language',
+        'status',
+        'order_no',
+        'created_at',
+        'updated_at',
+    ];
+}
---
+++ database/migrations/2025_08_14_124040_create_courses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('slug')->nullable();
+            $table->text('short_description')->nullable();
+            $table->text('about_course')->nullable();
+            $table->text('why_join_the_course')->nullable();
+            $table->string('instructor_name')->nullable();
+            $table->string('instructor_designation')->nullable();
+            $table->text('instructor_details')->nullable();
+            $table->float('mrp')->default(0.00)->nullable();
+            $table->float('sellable_price')->default(0.00)->nullable();
+            $table->tinyInteger('is_popular')->default(0)->nullable();
+            $table->tinyInteger('show_on_home')->default(0)->nullable();
+            $table->string('featured_image')->nullable();
+            $table->tinyInteger('is_certificate')->default(0)->nullable();
+            $table->string('certificate_file')->nullable();
+            $table->string('language')->nullable();
+            $table->tinyInteger('status')->default(0)->nullable();
+            $table->integer('order_no')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
